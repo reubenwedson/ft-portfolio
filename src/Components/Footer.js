a22fb11bce14805d75cd0f0413c28f8995265438
Reuben Wedson <reubenwedson@gmail.com>
2020-10-25 18:43:24 +0300

    Update Footer.js
    Signed-off-by: Reuben Wedson <reubenwedson@gmail.com>

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul>
-            <li>Modified with love by 23n <span>♥</span>️</li>
+            <li>Modified with by 23n</li>
            </ul>
 
            <ul className="copyright">
