b1ac76ee82e535d02626f29eea4a2b8404c31284
Reuben Wedson <reubenwedson@gmail.com>
2020-10-25 15:42:32 +0300

    Updated portfolio organization ⛑
    Signed-off-by: Reuben Wedson <reubenwedson@gmail.com>

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,67 +1,19 @@
 import React, { Component } from 'react';
-
+import placeholder from "../../public/images/placeholder.png"
 class Portfolio extends Component {
   render() {
 
     if(this.props.data){
-      var hostedAndEditedByMe = this.props.data.hostedAndEditedByMe.map(function(hostedAndEditedByMe){
-        var projectImage = 'images/portfolio/'+hostedAndEditedByMe.image;
-        return <div key={hostedAndEditedByMe.title} className="columns portfolio-item">
+        var works = this.props.data.works.map(function(work){
+        var projectImage = work.image ? 'images/portfolio/'+work.image : 'images/'+placeholder;
+        return <div key={work.title} className="columns portfolio-item">
            <div className="item-wrap">
-            <a href={hostedAndEditedByMe.url} title={hostedAndEditedByMe.title} target="_blank"  rel="noopener noreferrer">
-               <img alt={hostedAndEditedByMe.title} src={projectImage} />
+            <a href={work.url} title={work.title} target="_blank"  rel="noopener noreferrer">
+               <img alt={work.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{hostedAndEditedByMe.title}</h5>
-                     <p>{hostedAndEditedByMe.category}</p>
-                  </div>
-                </div>
-            </a>
-          </div>
-        </div>
-      })
-      var shotAndEditedByMe = this.props.data.shotAndEditedByMe.map(function(shotAndEditedByMe){
-        var projectImage = 'images/portfolio/'+shotAndEditedByMe.image;
-        return <div key={shotAndEditedByMe.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={shotAndEditedByMe.url} title={shotAndEditedByMe.title} target="_blank"  rel="noopener noreferrer">
-               <img alt={shotAndEditedByMe.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{shotAndEditedByMe.title}</h5>
-                     <p>{shotAndEditedByMe.category}</p>
-                  </div>
-                </div>
-            </a>
-          </div>
-        </div>
-      })
-      var shotByMe = this.props.data.shotByMe.map(function(shotByMe){
-        var projectImage = 'images/portfolio/'+shotByMe.image;
-        return <div key={shotByMe.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={shotByMe.url} title={shotByMe.title} target="_blank"  rel="noopener noreferrer">
-               <img alt={shotByMe.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{shotByMe.title}</h5>
-                     <p>{shotByMe.category}</p>
-                  </div>
-                </div>
-            </a>
-          </div>
-        </div>
-      })
-        var producedByMe = this.props.data.producedByMe.map(function(producedByMe){
-        var projectImage = 'images/portfolio/'+producedByMe.image;
-        return <div key={producedByMe.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={producedByMe.url} title={producedByMe.title} target="_blank"  rel="noopener noreferrer">
-               <img alt={producedByMe.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{producedByMe.title}</h5>
-                     <p>{producedByMe.category}</p>
+                 <h5>{work.title}</h5>
+                     <p>{work.category}</p>
                   </div>
                 </div>
             </a>
@@ -72,51 +24,14 @@ class Portfolio extends Component {
 
     return (
       <section id="portfolio">
-
       <div className="row">
 
          <div className="twelve columns collapsed">
 
-            <h1>Hosted and edited by me.</h1>
+            <h1>Works</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {hostedAndEditedByMe}
-            </div>
-          </div>
-      </div>
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
-            <h1>Shot and edited by me.</h1>
-
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {shotAndEditedByMe}
-            </div>
-          </div>
-      </div>
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
-            <h1>Shot by me.</h1>
-
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {shotByMe}
-            </div>
-          </div>
-      </div>
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
-            <h1>Produced by me.</h1>
-
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {producedByMe}
+                {works}
             </div>
           </div>
       </div>
